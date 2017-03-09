d7a022bfb55c4b05419d3ee606c4d9494d19a196
CoolCode2 <wcole2222@gmail.com>
2017-03-08 19:10:44 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("user Flipped" + " " + cardOne);
+console.log("User Flipped" + " " + cardFour);
